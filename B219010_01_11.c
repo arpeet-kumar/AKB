2a29fa4b705beab652330d275e88e1a8f6c66186
arpeet-kumar <57867936+arpeet-kumar@users.noreply.github.com>
2019-11-18 22:59:11 +0530

    Create B219010_01_11.c

---
+++ B219010_01_11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    printf("Enter 3 numbers in the format nn nn nn\n");
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>=b && a>=c)
+        printf("%d",a);
+    else if(b>=a && b>=c)
+        printf("%d",b);
+        else printf("%d",c);
+    return(0);
+}
