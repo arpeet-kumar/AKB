b19f772ee372a896ecfcc4761f91971cca02bc89
arpeet-kumar <57867936+arpeet-kumar@users.noreply.github.com>
2019-11-19 16:22:07 +0530

    Create B219010_02_06.c

---
+++ B219010_02_06.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a number\n");
+    scanf("%d",&a);
+    printf("The char is %c\n",a);
+    printf("The decimal is %d\n",a);
+    printf("The float is %f\n",a);
+    return 0;
+}
+
