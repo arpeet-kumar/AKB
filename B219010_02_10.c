2ce0b0c2682d31d7215d34c518581185731c1d10
arpeet-kumar <57867936+arpeet-kumar@users.noreply.github.com>
2019-11-19 16:23:52 +0530

    Create B219010_02_10.c

---
+++ B219010_02_10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a1,a2,a3,a4,a5,a6,a7,a8,a9;
+    printf("Enter 9 numbers in the format nn nn nn nn nn nn nn nn nn nn...\n");
+    scanf("%d %d %d %d %d %d %d %d %d",&a1,&a2,&a3,&a4,&a5,&a6,&a7,&a8,&a9);
+    printf("%d ,\t%d ,\t%d\n",a1,a2,a3);
+    printf("%d ,\t%d ,\t%d\n",a4,a5,a6);
+    printf("%d ,\t%d ,\t%d\n",a7,a8,a9);
+    return 0;
+}
+
