c55c5562083c779066cdae79a110dbd0f252492a
arpeet-kumar <57867936+arpeet-kumar@users.noreply.github.com>
2019-11-19 16:25:21 +0530

    Create B219010_03_02.c

---
+++ B219010_03_02.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    printf("Enter 2 numbers in the format nn nn\n");
+    scanf("%d %d",&x,&y);
+    printf("%d * %d = %d\n",x,y,x*y);
+    return 0;
+}
