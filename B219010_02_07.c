9e5607aeed7f377a13691f386c3a90c6e8107930
arpeet-kumar <57867936+arpeet-kumar@users.noreply.github.com>
2019-11-19 16:22:32 +0530

    Create B219010_02_07.c

---
+++ B219010_02_07.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    printf("PPPPP\tRRRRR\tIIIII\n");
+    printf("P   P\tR   R\t  I  \n");
+    printf("P   P\tR   R\t  I  \n");
+    printf("PPPPP\tRRRRR\t  I  \n");
+    printf("P\tRR\t  I  \n");
+    printf("P\tR R\t  I  \n");
+    printf("P\tR  R\tIIIII\n");
+    return 0;
+}
+
